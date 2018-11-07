23d42d83f326a27d5daf4afac0bd1318a68ba1d6
sk-ip <sudhanshukumar5459@gmail.com>
2018-11-07 17:19:40 +0530

    learned about spread and arrow functions

---
+++ index3.js
@@ -0,0 +1,14 @@
+let arr = [12, 23, 34];
+var arr2 = [1, 0, ...arr];
+console.log(arr2);
+/*
+function square(number)
+{
+    return number * number;
+}
+*/
+
+// arrow function
+
+const nameoffunction = (number1, number2) => number1 * number2;
+console.log(nameoffunction(10, 20));
